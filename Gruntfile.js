0ba2481adf2ead14797adcd985897be36466c2d4
Mark Lybrand <mlybrand@gmail.com>
2015-05-31 21:02:38 -0700

    added grunt-contrib-copy. configured to move bs and jq files to another directory. ran copy and created vendor dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,18 @@
 var grunt = require('grunt');
 
+grunt.initConfig({
+	copy: {
+		main: {
+			files: [
+				{ cwd: 'bower_components/bootstrap/dist/', src: '**/*', dest: 'vendor', expand: true },
+				{ cwd: 'bower_components/jquery/dist/', src: '*', dest: 'vendor/js', expand: true }
+			]
+		}
+	}
+});
+
+grunt.loadNpmTasks('grunt-contrib-copy');
+
 grunt.registerTask('default', function() {
 	console.log('task stub');
 });
